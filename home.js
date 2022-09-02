963e4fc6c99b66d91788196d8ae73e209cde4c92
jnile <jaidevnileshkumar2002@gmail.com>
2022-09-02 09:34:28 +0100

    first commit

---
+++ home.js
@@ -0,0 +1,25 @@
+const net = new brain.NeuralNetwork();
+
+net.train([
+    {
+        input: [0,0],
+        output: [0]
+    },
+    {
+        input: [0,1],
+        output: [1]
+    },
+    {
+        input: [1,0],
+        output: [1]
+    },
+    {
+        input: [1,1],
+        output: [0]
+    }
+]);
+
+const diagram = document.getElementById("diagram");
+diagram.innerHTML = brain.utilities.toSVG(net);
+
+console.log(net.run([0,0]));
